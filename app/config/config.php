57ce940033b2a38c045ef403e5d06819c51f4676
stanleyivan96 <stanleyivan96@gmail.com>
2022-08-17 16:32:08 +0800

    Loaded test views and config files

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/custommvc/');
+  // Site Name
+  define('SITENAME', 'CustomMVC');
