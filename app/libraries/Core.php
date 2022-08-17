dc590e9ad6a0633291988a9dec72b870b7dc8ab6
stanleyivan96 <stanleyivan96@gmail.com>
2022-08-17 13:01:11 +0800

    Setting up Core.php

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,1 +1,20 @@
-HELLO
+<?php
+  /*
+   * App Core Class
+   * Creates URL & loads core controller
+   * URL Format - /controller/method/params
+  */
+  class Core{
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
+
+    public function __construct(){
+      $this->getUrl();
+    }
+
+    public function getUrl(){
+      echo $_GET['url'];
+    }
+  }
+   
